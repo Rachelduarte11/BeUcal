7b6e9688695798cf9e21afac22de0a6921b42716
rachel11072003 <rachel11072003@gmail.com>
2023-10-02 19:22:13 -0500

    main agregado

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:becertus_proyecto/widgets/header_section.dart';
 import 'package:becertus_proyecto/widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {

1a3b5bc3fe3103e0668f869bed8f18987fd4022d
76287712 <76287712@certus.edu.pe>
2023-10-05 19:12:29 -0500

    Resolviendo problemas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:becertus_proyecto/screens/add_to_do.dart';
 import 'package:becertus_proyecto/screens/home_screen.dart';
-import 'package:becertus_proyecto/screens/login_screen.dart';
 import 'package:becertus_proyecto/widgets/header_section.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';

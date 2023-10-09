422316033700b64511c56e7148e9822abe14abea
rachel11072003 <rachel11072003@gmail.com>
2023-10-09 10:43:14 -0500

    se agrego la funcionalidad extendible

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -1,88 +1,189 @@
 import 'package:becertus_proyecto/models/colors.dart';
 import 'package:becertus_proyecto/screens/add_to_do.dart';
+/*import 'package:becertus_proyecto/screens/home_screen.dart';
+import 'package:becertus_proyecto/screens/performance_screen.dart';
+import 'package:becertus_proyecto/screens/profile.dart';*/
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-class CustomeNavigationBar extends StatelessWidget {
+class CustomeNavigationBar extends StatefulWidget {
   const CustomeNavigationBar({
     super.key,
   });
-//barra de menu
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(24.0), // Radio en la esquina superior izquierda
-        topRight: Radius.circular(24.0), // Radio en la esquina superior derecha
-      ),
-      child: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xFFE2E2E2),
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
-       // unselectedItemColor: greyDark,
-        selectedItemColor: greyDark,
-        items: [
-          
-          BottomNavigationBarItem(
-            icon: Icon(Icons.stacked_bar_chart_rounded,
-            size: 32, ),
-            label: 'Stack',
-          ),
 
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded,
-            size: 32,),
-            activeIcon: const Icon(
-              Icons.home_rounded,
-              color: Color.fromARGB(255, 255, 153, 153),
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline_outlined,
-            size: 32,
-            ),
-            
-            label: 'Play',
-          ),
-          BottomNavigationBarItem(
-            icon: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (builder)=> AddToDo()));
-              },
-              child: CustomAddIcon(
-                circleColor:
-                    Color(0xFFFD6A6A), // Cambia el color del círculo exterior
-                plusColor: Color.fromARGB(
-                    255, 255, 255, 255), // Cambia el color del signo "+"
-              ),
-            ),
-            label: 'Add',
-          )
-        ],
-      ),
-    );
-  }
+  @override
+  State<CustomeNavigationBar> createState() => _CustomeNavigationBarState();
 }
 
-class CustomAddIcon extends StatelessWidget {
-  final Color circleColor;
-  final Color plusColor;
-
-  CustomAddIcon({required this.circleColor, required this.plusColor});
-
+class _CustomeNavigationBarState extends State<CustomeNavigationBar> {
+  final isDialOpen = ValueNotifier(false);
+  int index = 0;
+ /* final List<Widget> _tabList = [
+    HomeScreen(),
+    MyPerformance(),
+    MyProfile(),
+  ];*/
+//barra de menu
   @override
-  Widget build(BuildContext context) {
-    return CircleAvatar(
-      backgroundColor: circleColor,
-      radius: 18.0, // Tamaño del círculo exterior
-      child: Center(
-        child: Icon(
-          Icons.add_rounded,
-          color: plusColor, // Color del signo "+"
-          size: 32.0, // Tamaño del signo "+"
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: () async {
+          if (isDialOpen.value) {
+            isDialOpen.value = false;
+            return false;
+          } else {
+            return true;
+          }
+        },
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft:
+                Radius.circular(24.0), // Radio en la esquina superior izquierda
+            topRight:
+                Radius.circular(24.0), // Radio en la esquina superior derecha
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor:const  Color(0xFFE2E2E2),
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            currentIndex: index,
+            unselectedItemColor: const Color.fromARGB(255, 107, 107, 107),
+            selectedItemColor: greyDark,
+            onTap: (int i) {
+              setState(() {
+                index = i;
+              });
+            },
+            items: [
+               const BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_rounded,
+                    size: 32,
+                  ),
+                  
+                  label: 'Home',
+                  backgroundColor: Colors.amber),
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.stacked_bar_chart_rounded,
+                  size: 32,
+                ),
+                label: 'Stack',
+              ),
+             
+              const BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_outline_outlined,
+                  size: 32,
+                ),
+                label: 'Play',
+              ),
+              BottomNavigationBarItem(
+                icon: InkWell(
+                  child: SpeedDial(
+                    buttonSize: const Size.fromRadius(20),
+                    backgroundColor: const Color(0xFFFD6A6A),
+                    overlayColor: Colors.black,
+                    overlayOpacity: 0.4,
+                    spaceBetweenChildren: 5,
+                    child:  Icon(
+                      Icons.add_rounded,
+                      size: 36,
+                    ),
+                    children: [
+                      SpeedDialChild(
+                        labelWidget: Container(
+                          width: 180,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 6.0,
+                              horizontal:
+                                  8), // Espacio alrededor del contenedor
+                          decoration: BoxDecoration(
+                            color: redStatic, // Color de fondo personalizado
+                            borderRadius: BorderRadius.circular(
+                                30.0), // Bordes redondeados
+                          ),
+                          child: const Text(
+                            'Calendario',
+                            style: TextStyle(
+                              fontFamily: 'Mitr',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18, // Tamaño de fuente personalizado
+                              color: Colors.white, // Color del texto
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (builder) => AddToDo()),
+                          );
+                        },
+                      ),
+                      SpeedDialChild(
+                        labelWidget: Container(
+                          width: 180,
+                          padding:const  EdgeInsets.all(
+                              4.0), // Espacio alrededor del contenedor
+                          decoration: BoxDecoration(
+                            color: redStatic, // Color de fondo personalizado
+                            borderRadius: BorderRadius.circular(
+                                30.0), // Bordes redondeados
+                          ),
+                          child: const Text(
+                            'Nuevo Evento',
+                            style: TextStyle(
+                              fontFamily: 'Mitr',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18, // Tamaño de fuente personalizado
+                              color: Colors.white, // Color del texto
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (builder) => AddToDo()),
+                          );
+                        },
+                      ),
+                      SpeedDialChild(
+                        labelWidget: Container(
+                          width: 180,
+                          padding: const EdgeInsets.all(
+                              4.0), // Espacio alrededor del contenedor
+                          decoration: BoxDecoration(
+                            color: redStatic, // Color de fondo personalizado
+                            borderRadius: BorderRadius.circular(
+                                30.0), // Bordes redondeados
+                          ),
+                          child: const Text(
+                            'Tareas',
+                            style: TextStyle(
+                              fontFamily: 'Mitr',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18, // Tamaño de fuente personalizado
+                              color: Colors.white, // Color del texto
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (builder) => AddToDo()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                label: 'Add',
+              )
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
---
+++ lib/widgets/routes.dart
@@ -0,0 +1,20 @@
+/*import 'package:becertus_proyecto/screens/home_screen.dart';
+import 'package:becertus_proyecto/screens/performance_screen.dart';
+import 'package:becertus_proyecto/screens/profile.dart';
+import 'package:flutter/material.dart';
+
+class Routes extends StatelessWidget {
+  final int index;
+  const Routes({super.key, required Column child});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> myList = [
+      const HomeScreen(),
+      const MyPerformance(),
+      const MyProfile(),
+    ];
+    return myList[index];
+  }
+}
+*/

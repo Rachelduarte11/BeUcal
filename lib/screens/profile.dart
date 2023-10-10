4c92f9ecee0abb0006647726c96b35c87ed665ef
Alessandro Marquina <112639704+76287712@users.noreply.github.com>
2023-10-10 17:21:46 -0500

    Cambios desde el github

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyProfile extends StatelessWidget {
-  const MyProfile({Key? key});
+  const MyProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +10,27 @@ class MyProfile extends StatelessWidget {
         children: [
           ClipPath(
             clipper: CustomClipPath(),
+            //Contenedor superior (Celeste)
             child: Container(
               height: 300,
               color: Color.fromARGB(255, 73, 196, 223),
               child: Stack(
                 children: [
-                  Positioned(
+                  const Positioned(
+                    //Texto (PERFIL)
                     left: 30,
                     top: 45,
-                    child: Text("Perfil",
+                    child: Text(
+                      "Perfil",
                       style: TextStyle(
-                        color: const Color(0xFF323232),
+                        color: Color(0xFF323232),
                         fontSize: 40,
                         fontWeight: FontWeight.w500,
                         fontFamily: "Mitr",
                       ),
                     ),
                   ),
+                  //Icono (PENCIL)
                   Positioned(
                     bottom: 100,
                     left: 220,
@@ -40,15 +44,18 @@ class MyProfile extends StatelessWidget {
               ),
             ),
           ),
+          //Contenedor Padre (blanco)
           Transform.translate(
-            offset: Offset(0, -190), // Ajusta la posición vertical del segundo contenedor
+            offset: const Offset(
+                0, -190), // Ajusta la posición vertical del segundo contenedor
             child: Stack(
               children: [
                 Container(
                   width: 330,
-                  height: 320,
+                  height: 340,
                   decoration: BoxDecoration(
                     color: Colors.white,
+                    //Bordes del contenedor
                     borderRadius: BorderRadius.circular(15),
                     boxShadow: [
                       BoxShadow(
@@ -56,34 +63,67 @@ class MyProfile extends StatelessWidget {
                             Color.fromARGB(255, 161, 161, 161).withOpacity(0.4),
                         spreadRadius: 5,
                         blurRadius: 6,
-                        offset: Offset(0, 3),
+                        offset: const Offset(0, 3),
                       ),
                     ],
                   ),
+
+                  //Texto Usuario
                   child: const Center(
                     child: Positioned(
-                      child: Text("Alessandro Miguel Arias Marquina",
-                          style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700),
-                          textAlign: TextAlign.center,
+                      child: Text(
+                        "Alessandro Miguel Arias Marquina",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.center,
                       ),
                     ),
                   ),
                 ),
-                //Contenedor hijo
+                //Contenedor hijo (Gris)
                 Positioned(
-                  top: 210, 
-                  left: 15, 
+                  top: 210,
+                  left: 15,
                   child: Container(
-                    width: 300,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 234, 234, 234),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
+                      width: 300,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 234, 234, 234),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Transform.translate(
+                        offset: const Offset(0, -170),
+                        child: Center(
+                          child: Container(
+                            width: 95, // Aumenta el tamaño del contenedor
+                            height: 18, // Aumenta el tamaño del contenedor
+                            decoration: BoxDecoration(
+                              color: Colors.grey, // Establece el fondo en gris
+                              borderRadius: BorderRadius.circular(40), // Radio para el contenedor circular
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                ),
+                              ],
+                            ),
+                            child: Center(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(40),
+                                child: Image.asset(
+                                  "assets/images/StackPath.jpeg",
+                                  width: 90, 
+                                  height: 180, 
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )),
                 ),
               ],
             ),

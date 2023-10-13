94355e2338528faef08e134afcca58b2c33cb61c
rachel11072003 <rachel11072003@gmail.com>
2023-10-13 15:10:00 -0500

    las dos screens principales agregadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:becertus_proyecto/screens/Started/screen_1.dart';
+import 'package:becertus_proyecto/screens/home_screen.dart';
 import 'package:becertus_proyecto/screens/profile.dart';
 import 'package:becertus_proyecto/widgets/header_section.dart';
 import 'package:flutter/material.dart';
@@ -13,14 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false, // Pasa la instancia de User
+      debugShowCheckedModeBanner: false, 
       title: 'Flutter demo',
       theme: ThemeData(
           textTheme: Theme.of(context).textTheme.apply(
                 bodyColor: Color.fromARGB(255, 230, 230, 230),
                 displayColor: Colors.white,
               )),
-      home: MyProfile(),
+      home: OneScreen(),
       getPages: [
         GetPage(name: '/', page: () => const CustomAppBar()),
         // GetPage(name: '/song', page: ()=> const SongScreen()),

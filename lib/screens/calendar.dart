7d41e97c9b68caecb83ae10dc0ee340e0fc6e63d
76287712 <76287712@certus.edu.pe>
2023-11-14 19:27:38 -0500

    Avance de Screen de calendario

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -4,8 +4,26 @@ import 'package:becertus_proyecto/widgets/chip_data.dart';
 import 'package:becertus_proyecto/widgets/header_section.dart';
 import 'package:flutter/material.dart';
 
-class Calendar extends StatelessWidget {
-  const Calendar({super.key});
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+
+  int selectedND = 0; // 0: Todos, 1: ND1, 2: ND2, 3: ND3
+
+  int selectedButton = 0;
+
+  Color colorND1 = const Color(0xFF2D94AA);
+  Color colorND2 = const Color(0XFFE7E2E2);
+  Color colorND3 = const Color(0XFFE7E2E2);
+
+  Color textColorND1 = Colors.white;
+  Color textColorND2 = Colors.black;
+  Color textColorND3 = Colors.black;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +32,217 @@ class Calendar extends StatelessWidget {
       appBar: CustomAppBar(),
       body: ListView(
         children: [
+          SizedBox(height: 15),
+          Container(
+            height: 400,
+            width: 0,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+              boxShadow: [
+                BoxShadow(blurRadius: 2, spreadRadius: 0, color: Colors.grey),
+              ],
+            ),
+            padding: EdgeInsets.only(top: 25),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 25),
+                    const Column(
+                      children: [
+                        Text(
+                          "Noviembre, 2023",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Arimo",
+                            fontSize: 23,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(width: 80),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedButton = 0;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                color: selectedButton == 0
+                                    ? Color(0xFF2D94AA)
+                                    : Color.fromARGB(255, 244, 242, 242),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: Icon(
+                              Icons.keyboard_arrow_left,
+                              size: 30,
+                              color: selectedButton == 0
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedButton = 1;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                color: selectedButton == 1
+                                    ? Color(0xFF2D94AA)
+                                    : Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: Icon(
+                              Icons.keyboard_arrow_right,
+                              size: 30,
+                              color: selectedButton == 1
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                //Segunda fila
+                Container(
+                  height: 30,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 0.1,
+                          spreadRadius: 0,
+                        )
+                      ]),
+                  child: Center(
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedND = 0;
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 66.6,
+                            decoration: BoxDecoration(
+                              color: selectedND == 0
+                                  ? Color(0xFF2D94AA)
+                                  : Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "ND1",
+                                style: TextStyle(
+                                  color: selectedND == 0
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 14,
+                                  fontFamily: "Mitr",
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedND = 1;
+                            });
+                          },
+                          child: Container(
+                            width: 66.6,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: selectedND == 1
+                                  ? Color(0xFF2D94AA)
+                                  : Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "ND2",
+                                style: TextStyle(
+                                  color: selectedND == 1
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 14,
+                                  fontFamily: "Mitr",
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedND = 2;
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 66.6,
+                            decoration: BoxDecoration(
+                              color: selectedND == 2
+                                  ? Color(0xFF2D94AA)
+                                  : Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "ND3",
+                                style: TextStyle(
+                                  color: selectedND == 2
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 14,
+                                  fontFamily: "Mitr",
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          //Lllamando al Widgets de Tareas pendientes ---
           Center(
             child: Titles(
               text: 'Tareas Pendientes',
